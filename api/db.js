4f3929d7ab9f24cd422fa9573ec2484a4db6e7c8
jianfeizhu <jianfeizhu2516@gmail.com>
2024-07-22 12:41:10 -0400

    fix vercel

--- api/db.js
+++ api/db.js
@@ -1,6 +1,11 @@
 import mysql from 'mysql2';
 import dotenv from 'dotenv'
 dotenv.config();
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_PORT:', process.env.DB_PORT);
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
