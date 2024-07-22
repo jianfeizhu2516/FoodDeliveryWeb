14c5d10341f04721031c2023450b2e37f1e59b12
jianfeizhu <jianfeizhu2516@gmail.com>
2024-07-22 11:34:58 -0400

    fix routing issue

--- api/index.js
+++ api/index.js
@@ -10,6 +10,10 @@ app.use(express.json())
 
 app.use("/api/products", productRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
 app.listen(8800, () => { 
   console.log('server at 8800');
 });
