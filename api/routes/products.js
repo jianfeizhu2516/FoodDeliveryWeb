f069f50c3b0da4fa8eed72d72c60d600cdb52d45
jianfeizhu <jianfeizhu2516@gmail.com>
2024-08-09 16:41:55 -0400

    test deployment

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,7 +2,9 @@ import express from "express";
 import { fetchAllProducts } from "../controllers/productController.js";
 
 const router = express.Router();
-
+router.get('/', (req, res) => {
+    res.send('Product list'); // 测试返回
+  });
 router.get("/",fetchAllProducts)
 
 export default router;
