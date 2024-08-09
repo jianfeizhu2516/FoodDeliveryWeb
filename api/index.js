25d1588a0891c4d2fabb2a392ad1bae3f28bf0c6
jianfeizhu <jianfeizhu2516@gmail.com>
2024-08-09 15:00:15 -0400

    allow all origin

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: ' https://cors-anywhere.herokuapp.com/https://food-delivery-web-peach.vercel.app/',
+  origin: '*',
   credentials: true,
 }));
 app.use(cookieParser());
