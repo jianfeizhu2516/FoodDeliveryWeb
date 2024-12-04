5c836ee9bbbe7b62a9629e8d5f51d31eafcb8cfe
jianfeizhu <jianfeizhu2516@gmail.com>
2024-12-04 15:24:46 -0500

    fix signup and login

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,9 +1,27 @@
 import db from "../db.js"
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import { doc, setDoc, addDoc, collection } from "firebase/firestore";
-export const login = (req, res) => {
+import { doc, setDoc, addDoc, collection, getDoc, query, where, getDocs } from "firebase/firestore";
+export const login = async (req, res) => {
+    const { email, password } = req.body;
 
+    const usersRef = collection(db, 'users');
+    const q = query(usersRef, where('email', '==', email));
+    const querySnapshot = await getDocs(q);
+
+    if(querySnapshot.empty){
+        return res.status(404).json({ message: 'User not found!' })
+    }
+
+    const isPasswordCorrect = bcrypt.compareSync(password, querySnapshot.docs[0].data().password);
+
+    if (!isPasswordCorrect) return res.status(400).json({ message: "Wrong username or password!" });
+    
+    const token = jwt.sign({ email: req.body.email }, "jwtkey");
+
+    res.cookie('access_token', token, { httpOnly: true })
+
+    return res.status(200).json({ message: "Login successul" })
     // const q = 'SELECT * FROM users WHERE email = ?';
 
     // db.query(q, [req.body.email], (err, results) => {
@@ -25,43 +43,31 @@ export const login = (req, res) => {
 
 export const signup = async (req, res) => {
 
-    const q = 'SELECT * FROM users WHERE email = ?';
+    const { email, password } = req.body;
+
+    const usersRef = collection(db, 'users');
+    const q = query(usersRef, where('email', '==', email));
+    const querySnapshot = await getDocs(q);
+
+    if (!querySnapshot.empty) {
+        return res.json({ message: "Email has alrady been used" })
+    }
 
     const salt = bcrypt.genSaltSync(10);
 
-    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
-
-    const { email, password } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, salt);
 
     const token = jwt.sign({ email }, "jwtkey");
-    try{
+    try {
         await addDoc(collection(db, "users"), {
-            username: email,
+            email: email,
             password: hashedPassword
         })
-    }catch(err){
-        console.log('sign up failed',err)
+        return res.status(201).cookie("access_token", token, {
+            httpOnly: true
+        }).json({ message: 'User registration successful' })
+    } catch (err) {
+        console.log('sign up failed', err)
     }
-
-
-    // db.query(q, req.body.email, (err,results)=>{
-    //     if(err){
-    //       return res.status(500).json({ error: err.message });
-    //     }
-    //     if(results?.length > 0)
-    //     {   
-    //         return res.json({message:"Email has alrady been used"})
-    //     }
-    //     const insertUserQuery = 'INSERT INTO users (email, password) VALUES (?, ?)';
-
-    //     db.query(insertUserQuery,[req.body.email,hashedPassword],(err,results)=>{
-    //         if(err){
-    //             return res.status(500).json({error:err.message});
-    //         }
-    //         return res.status(201).cookie("access_token",token,{
-    //             httpOnly:true
-    //         }).json({message:'User registration successful'})
-    //     })
-    // }) 
 
 }
--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ app.use(cors({
 }));
 
 app.use(cookieParser());
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
