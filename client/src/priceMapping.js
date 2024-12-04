fb850868cb4836e21d5bf4596bc0f9501d412edc
jianfeizhu <jianfeizhu2516@gmail.com>
2024-12-04 13:39:39 -0500

    add product pricing id

--- client/src/priceMapping.js
+++ client/src/priceMapping.js
@@ -1,7 +1,13 @@
 const priceMapping = {
     '1': 'price_1PlcXKAAMFTLuMMi178YxJ7q',  //veggie pizza
     '2': 'price_1PlcWPAAMFTLuMMixmAa57e8', //beef burger
+    '3':'price_1QSNHqAAMFTLuMMiDw8C0mfE',//homemade bread
+    '4':'price_1QSNRZAAMFTLuMMi68SY3f49',//cheesy bacon
+    '5':'price_1QSNS1AAMFTLuMMic2CE3uzm',//mozarella pizza
+    '6':'price_1QSNSCAAMFTLuMMiIuLlhpsC'//mushroom pizza
     // add other mappings here
   };
   
   export default priceMapping;
+
+  
